75e053ca8c2e01429be1ccf1bc9bb0c446ba4f12
yuy415 <yuyan@hsph.harvard.edu>
2024-03-04 22:46:02 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,7 +103,7 @@ chart_base = alt.Chart(source).properties(
           ).add_selection(selector
                           ).transform_lookup(
     lookup="id",
-    from_=alt.LookupData(subset, 'country-code', ['Country','Year','Cases']),
+    from_=alt.LookupData(subset_std, 'country-code', ['Country','Year','Cases']),
 ).properties(
     title='STD cases worldwide in {year}'
 )
